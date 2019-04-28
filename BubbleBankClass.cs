2cd4c33f2ff305af81510207a7b2c0dbe2ff6f89
Jeroen Broks <facebook@tbbs.nl>
2019-04-28 21:57:01 +0200

    Create BubbleBankClass.cs

---
+++ BubbleBankClass.cs
@@ -0,0 +1,18 @@
+/* 
+   This library will add banking to Bubble.
+   As well as an endian conversion routine in case 
+   it's needed
+*/
+
+namespace Bubble {
+      
+      
+      internal class TBank {
+      }
+      
+      // This routine will be used for the actual API;
+      class BubbleBank {
+           private Dictionary<int,TBank> BankDict = new Dictionary<int,TBank>();
+      }
+
+}
