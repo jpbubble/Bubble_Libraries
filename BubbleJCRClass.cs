0ec152177788d1be147d529d1ee74c712d6c8dc6
Jeroen P. Broks <facebook@tbbs.nl>
2019-06-01 14:43:18 +0200

    vUpdate and unixification of file

--- BubbleJCRClass.cs
+++ BubbleJCRClass.cs
@@ -6,8 +6,9 @@
 // Mozilla Public License, v. 2.0. If a copy of the MPL was not
 // distributed with this file, You can obtain one at
 // http://mozilla.org/MPL/2.0/.
-// Version: 19.05.09
+// Version: 19.06.01
 // EndLic
+
 
 
 
@@ -175,6 +176,7 @@ namespace Bubble {
         #endregion
     }
 }
+
 
 
 
